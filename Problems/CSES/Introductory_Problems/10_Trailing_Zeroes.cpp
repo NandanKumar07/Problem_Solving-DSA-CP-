0ca31bcbda525143b6a7f61a2d54f229085072ab
NandanKumar07 <onlinestudy641@gmail.com>
2023-11-18 18:54:48 +0530

    H

--- Problems/CSES/Introductory_Problems/10_Trailing_Zeroes.cpp
+++ Problems/CSES/Introductory_Problems/10_Trailing_Zeroes.cpp
@@ -22,16 +22,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int trailing_zeroes(int n) {
-    int count = 0;
-    for (int i = 5; n / i >= 1; i *= 5) {
-        count += n / i;
-    }
-    return count;
-}
-
 int main() {
     int n;
     cin >> n;
-    cout << trailing_zeroes(n) << endl;
+    int ans = 0;
+    while(n > 0) {
+        ans += n/5;
+        n = n/5;
+    }
+    cout<<ans<<endl;
 }
